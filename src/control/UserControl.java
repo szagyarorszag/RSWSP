84be27d2465b6f2e7078d7261219f86971b2fd6a
szagyarorszag <bakitzhana566@gmail.com>
2023-12-26 19:41:18 +0600

    login ve been updated

--- src/control/UserControl.java
+++ src/control/UserControl.java
@@ -1,5 +1,34 @@
 package control;
 import szgPART.User;
-public class UserControl extends User {
+import szgPART.Database;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class UserControl extends User {
+    public static void main(String [] args) {
+    }
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static boolean isLoginAndPasswordCorrect(String login, String password){
+        for ( User user : Database.users ) {
+            if( user.getLogin().equals(login) && user.getPassword().equals(password)) return true;
+        }
+        return false;
+    }
+    public static boolean login() throws IOException {
+        System.out.println("Oral sufferings is started" );
+        while(true) {
+            System.out.println("Enter login");
+            String login = br.readLine();
+            System.out.println("Enter password");
+            String password = br.readLine();
+            if (!isLoginAndPasswordCorrect(login, password)) continue;
+
+
+
+
+        }
+
+    }
 }
